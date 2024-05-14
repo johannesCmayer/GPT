b6bfc48b439bf0941702fa9d5b53ae73735d0146
Johannes C. Mayer <j.c.mayer240@gmail.com>
2024-05-15 01:16:25 +0200

    f

--- app.nix
+++ app.nix
@@ -1,9 +1,10 @@
 { pkgs ? import <nixpkgs> {}, gsay }:
 
 pkgs.python310Packages.buildPythonPackage rec {
-  name = "gsay";
+  name = "gpt-ui";
   src = ./.;
   propagatedBuildInputs = with pkgs.python310Packages; [
+    gsay.packages.x86_64-linux.default
     pkgs.python310
     pyyaml
     xdg-base-dirs
