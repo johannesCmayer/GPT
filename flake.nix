64bf31b17f09135ea8746b8b92b8638608432b93
Johannes C. Mayer <j.c.mayer240@gmail.com>
2024-05-15 01:09:21 +0200

    flake2

--- flake.nix
+++ flake.nix
@@ -1,11 +1,15 @@
 {
   description = "gpt-ui";
   inputs = {
-    gsay.url = "github:johannesCmayer/gsay"
+    nixpkgs.url = "nixpkgs/nixos-23.11";
+    gsay.url = "github:johannesCmayer/gsay";
   };
   outputs = { self, nixpkgs, gsay }: {
     packages.x86_64-linux.default =
       # Notice the reference to nixpkgs here.
       with import nixpkgs { system = "x86_64-linux"; };
+      (pkgs.writeShellScriptBin "hello-man" ''
+        echo hello there
+      '');
   };
 }
