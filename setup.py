e0fc152e48b7030eef7fb03828e27f412da291b6
Johannes C. Mayer <j.c.mayer240@gmail.com>
2024-05-16 21:08:50 +0200

    refactor setuptools

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@
 
 from setuptools import setup, find_packages
 
-setup(name='gsay',
+setup(name='gpt-ui',
       version='1.0',
       # Modules to import from other scripts:
       packages=find_packages(),
-      package_data={'': ['license.txt']},
+      package_data={'gpt-ui': ['*.yaml']},
       include_package_data=True,
+      install_requires=[],
       # Executables
       scripts=["gpt-ui"],
      )
