529665288f8e609f763018e3aee8e7d55ae5a16c
Johannes C. Mayer <j.c.mayer240@gmail.com>
2024-05-16 20:43:08 +0200

    include test

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(name='gsay',
       version='1.0',
       # Modules to import from other scripts:
       packages=find_packages(),
+      package_data={'': ['license.txt']},
       include_package_data=True,
       # Executables
       scripts=["gpt-ui"],
